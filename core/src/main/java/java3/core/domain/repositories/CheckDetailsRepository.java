d8bf9e0f3485a49e3a662a26b76ef8b836deb35a
oviski <valentina.vekslere@gmail.com>
2017-11-22 16:47:22 +0200

    Domain. Exceptions, Core-api Dtos

---
+++ core/src/main/java/java3/core/domain/repositories/CheckDetailsRepository.java
@@ -0,0 +1,8 @@
+package java3.core.domain.repositories;
+
+import java3.core.domain.CheckDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CheckDetailsRepository extends JpaRepository<CheckDetails, Long> {
+
+}
---
+++ core/src/main/java/java3/core/domain/repositories/CheckRepository.java
@@ -0,0 +1,8 @@
+package java3.core.domain.repositories;
+
+import java3.core.domain.Check;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CheckRepository extends JpaRepository<Check, Long> {
+
+}
