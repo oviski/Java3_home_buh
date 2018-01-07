c04614a0241662d92553452dfbd8b889bdb0e73d
oviski <valentina.vekslere@gmail.com>
2018-01-07 22:02:27 +0200

    Add new command GetAllCheck

--- public-app/src/main/java/java3/app/jms/responsehandlers/JMSRegisterUserResponseHandler.java
+++ public-app/src/main/java/java3/app/jms/responsehandlers/JMSRegisterUserResponseHandler.java
@@ -3,6 +3,7 @@ package java3.app.jms.responsehandlers;
 import java3.app.common.JsonMapper;
 import java3.core.api.jms.JMSResponse;
 import java3.core.api.jms.SupportedCommandId;
+import java3.core.api.jms.requests.checks.JMSRegisterCheckResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -21,10 +22,10 @@ class JMSRegisterUserResponseHandler {
     @Override
     public ResponseEntity process(JMSResponse jmsResponse) {
         String payload = jmsResponse.getPayload();
-        JMSRegisterUserResponse response = jsonMapper.mapToObject(
-                payload, JMSRegisterUserResponse.class
+        JMSRegisterCheckResponse response = jsonMapper.mapToObject(
+                payload, JMSRegisterCheckResponse.class
         );
-        return ResponseEntity.ok(response.getUserDTO());
+        return ResponseEntity.ok(response.getCheckDTO());
     }
 
 }
